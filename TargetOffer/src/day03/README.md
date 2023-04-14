688486795961c56813ad9fea093ad89516bd8b45
BanishedWay <xujinqi2001@outlook.com>
2023-04-14 09:55:55 +0800

    Day03

---
+++ TargetOffer/src/day03/README.md
@@ -0,0 +1,5 @@
+# Day03
+
+- [替换空格（原题）](https://leetcode.cn/problems/ti-huan-kong-ge-lcof/) [ReplaceSpace](./ReplaceSpace.java)
+
+- [左旋转字符串（原题）](https://leetcode.cn/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof/) [ReverseLeftWords](./ReverseLeftWords.java)
---
+++ TargetOffer/src/day03/ReplaceSpace.java
@@ -0,0 +1,16 @@
+package day03;
+
+public class ReplaceSpace {
+    //替换空格
+    public String replaceSpace(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                sb.append("%20");
+            } else {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
