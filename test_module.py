485bb7d0bac2e5c6b985a0ee78a5b93b7f69cb3c
BanishedWay <xujinqi2001@outlook.com>
2023-04-23 19:53:15 +0800

    添加错误处理

--- test_module.py
+++ test_module.py
@@ -17,5 +17,23 @@ def test():
         print("Too many arguments!")
 
 
+class Student(object):
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return "Student(%s, %s)" % (self.name, self.age)
+
+    def __repr__(self):
+        return "Student(%s, %s)" % (self.name, self.age)
+
+    def __eq__(self, other):
+        return self.name == other.name and self.age == other.age
+
+
 if __name__ == "__main__":
     test()
+    s = Student("BanishedWay", 18)
+    print(s)
