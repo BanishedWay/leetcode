9024f60d39c5741fde867954352f4dff6d4af9b1
BanishedWay <xujinqi2001@outlook.com>
2023-05-11 16:09:15 +0800

    添加TCp

---
+++ TCP&UDP/TCP/server.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'TCP连接服务器'
+
+__author__ = 'BanishedWay'
+
+import socket, time
+
+
+def tcplink(sock, addr):
+    print('Accept new connection from %s:%s...' % addr)
+    sock.send(b'Welcome')
+    while True:
+        data = sock.recv(1024)
+        time.sleep(1)
+        if not data or data.decode('utf-8') == 'exit':
+            break
+        sock.send(('Hello %s' % data.decode('utf-8')).encode('utf-8'))
+    sock.close()
+    print('Connection from %s:%s closed' % addr)
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 监听端口
+s.bind(('127.0.0.1', 9999))
+s.listen(5)
+print('Waiting for connnection...')
+
+# 使用死循环接收来自客户端的连接
+while True:
+    sock, addr = s.accept()
+    t = threading.Thread(target=tcplink, args=(sock, addr))
+    t.start()
