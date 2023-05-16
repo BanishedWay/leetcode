59c0ebddc44611642b6f20d7ec36d83536f0b31c
BanishedWay <xujinqi2001@outlook.com>
2023-05-16 20:31:02 +0800

    添加WSGI接口

--- HTML/hello.py
+++ HTML/hello.py
@@ -5,6 +5,11 @@
 __author__ = 'BanishedWay'
 
 
+# 读取PATH_INFO
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    return [b'<h1>Hello, web!</h1>']
+    # return [b'<h1>Hello, web!</h1>']
+    # 在environ中读取PATH_INFO
+    body = '<h1>Hello, %s!<h1>' % (
+        environ['PATH_INFO'].encode('iso-8859-1').decode('utf8')[1:] or 'web')
+    return [body.encode('utf-8')]
