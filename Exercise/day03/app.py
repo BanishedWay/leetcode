96de632ab4217608689b475546959592ea48944a
BanishedWay <xujinqi2001@outlook.com>
2023-06-14 21:53:57 +0800

    修改密码生成器

--- Exercise/day03/app.py
+++ Exercise/day03/app.py
@@ -12,43 +12,70 @@ __author__ = 'BanishedWay'
 """
 
 import random
+import sys
+import string
 
 
-# 生成大写字母
-def get_upper():
-    # 随机生成至少一个大写字母
-    count = random.randint(1, 3)
-    return random.choices('ABCDEFGHIJKLMNOPQRSTUVWXYZ', k=count)
+# # 生成大写字母
+# def get_upper():
+#     # 随机生成至少一个大写字母
+#     count = random.randint(1, 3)
+#     return random.choices('ABCDEFGHIJKLMNOPQRSTUVWXYZ', k=count)
 
 
-# 生成特殊字符
-def get_special():
-    # 随机生成至少一个特殊字符
-    count = random.randint(1, 3)
-    return random.choices('!@#$%^&*()_+~', k=count)
+# # 生成特殊字符
+# def get_special():
+#     # 随机生成至少一个特殊字符
+#     count = random.randint(1, 3)
+#     return random.choices('!@#$%^&*()_+~', k=count)
 
 
-# 生成特定数目的小写字母和数字
-def get_lower(count):
-    string = 'abcdefghijklmnopqrstuvwxyz0123456789'
-    return random.choices(string, k=count)
+# # 生成特定数目的小写字母和数字
+# def get_lower(count):
+#     string = 'abcdefghijklmnopqrstuvwxyz0123456789'
+#     return random.choices(string, k=count)
 
 
-# 生成特定长度的密码
-def get_password(length):
+# # 生成特定长度的密码
+# def get_password(length):
+#     if length < 8:
+#         length = 8
+#     password = []
+
+#     upper_list = get_upper()
+#     special_list = get_special()
+#     password.extend(upper_list)
+#     password.extend(special_list)
+
+#     surplus_length = length - len(password)
+#     lower_list = get_lower(surplus_length)
+#     password.extend(lower_list)
+
+#     # 打乱顺序
+#     random.shuffle(password)
+#     return ''.join(password)
+
+
+# 修改版
+def get_password(length=8):
     if length < 8:
         length = 8
-    password = []
+    charactors = string.ascii_letters + string.digits + string.punctuation
 
-    upper_list = get_upper()
-    special_list = get_special()
-    password.extend(upper_list)
-    password.extend(special_list)
+    while True:
+        password = ''.join(random.choice(charactors) for i in range(length))
+        if any(c.islower() for c in password) and any(
+                c.isupper() for c in password) and any(
+                    c.isdigit()
+                    for c in password) and any(c in string.punctuation
+                                               for c in password):
+            return password
 
-    surplus_length = length - len(password)
-    lower_list = get_lower(surplus_length)
-    password.extend(lower_list)
 
-    # 打乱顺序
-    random.shuffle(password)
-    return ''.join(password)
+if __name__ == '__main__':
+    lst = sys.argv
+    if len(lst) <= 1:
+        length = 8
+    else:
+        length = int(sys.argv[1])
+    print(get_password(length))
